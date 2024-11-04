fb1a507eeba8787cfd510f4d63d234da9da61046
ANSHIKA KUMARI <101940406+Anshika2022@users.noreply.github.com>
2024-11-04 22:31:41 +0530

    Create String Compression III.java

---
+++ String Compression III.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String compressedString(String word) {
+        int count = 0;
+        char curr ='0';
+        StringBuilder sb = new StringBuilder();
+        for(char c: word.toCharArray()){
+            if(count<9 && c==curr){
+                count+=1;
+            }else{
+                if(count>0){
+                    sb.append(count);
+                    sb.append(curr);
+                }
+                curr = c;
+                count =1;
+            }
+        }
+        sb.append(count);
+        sb.append(curr);
+        return sb.toString();
+    }
+}
